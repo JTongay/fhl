996eec5a311fb639ff53c5935071cb903ed03f0d
Joseph Tongay <joseph.tongay@gmail.com>
2024-08-24 13:48:41 -0700

    starting to make the scheduler and fixing the events data. I feel like doing some front end stuff now

--- packages/core/migrations/1722717606672_fix_events_use_event_type.mjs
+++ packages/core/migrations/1722717606672_fix_events_use_event_type.mjs
@@ -4,10 +4,15 @@ import { Kysely } from "kysely";
  * @param db {Kysely<any>}
  */
 export async function up(db) {
+    await db.schema.alterTable("events")
+    .addColumn("start_at", "datetime")
+    .addColumn("season_id", "integer", (col) => col.references("seasons.id").onDelete("cascade"))
+    .execute()
 }
 
 /**
  * @param db {Kysely<any>}
  */
 export async function down(db) {
+    await db.schema.alterTable("events").dropColumn("start_at").dropColumn("season_id").execute()
 }
