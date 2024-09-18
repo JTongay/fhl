576371e1a145c3c93a864f69d919096ef93ea413
Joseph Tongay <joseph.tongay@gmail.com>
2024-09-17 19:11:59 -0700

    Cleaned up apollo config. I guess it works

--- apollo.config.mjs
+++ apollo.config.mjs
@@ -1,5 +1,4 @@
 const path = require("path");
-// console.log(__dirname);
 
 module.exports = {
   client: {
@@ -14,17 +13,3 @@ module.exports = {
     excludes: ["**/node_modules/**"],
   },
 };
-
-// export default {
-//   client: {
-//     service: {
-//       name: "fhl-web",
-//       localSchemaFile: path.join(
-//         __dirname,
-//         "packages/fhl-web-2/schema/schema.graphql"
-//       ),
-//     },
-//     // includes: ["src/**/*.tsx", "src/**/*.ts", "operations/**/*.graphql"],
-//     // excludes: ["**/node_modules/**"],
-//   },
-// };
