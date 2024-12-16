46916334ecc0284c72f4a85080a628dc706490bd
Joseph Tongay <joseph.tongay@gmail.com>
2024-12-15 18:58:48 -0800

    killed the old react app

--- packages/fhl-web/app/league/error.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import { PropsWithChildren } from "react";
-
-export default function Error(props: PropsWithChildren) {
-    console.log(props)
-
-    return <h1>Error boundary in League</h1>
-}
--- packages/fhl-web/operations/mutations/loginMutation.tsx
+++
@@ -1,7 +0,0 @@
-import { graphql } from "@/generated/gql";
-
-export const LOGIN_MUTATION = graphql(/* GraphQL */`
-    mutation login() {
-        token
-    }
-`)
